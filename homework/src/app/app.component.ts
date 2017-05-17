05bde8f941f1e7cfd1842dcc95eb64269420fcb1
psokolov <psokolov@payneteasy.com>
2017-05-17 16:31:52 +0300

    components and images

--- homework/src/app/app.component.ts
+++ homework/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'homework-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- homework/src/app/app.module.ts
+++ homework/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { SocnetGroupComponent } from './socnet-group/socnet-group.component';
+import { MainListComponent } from './main-list/main-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemperatureComponent,
+    SocnetGroupComponent,
+    MainListComponent
   ],
   imports: [
     BrowserModule,
