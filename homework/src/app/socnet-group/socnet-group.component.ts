80eb5db3ea237b89099c444b2bbdb1d368cfcdff
psokolov <psokolov@payneteasy.com>
2017-05-17 20:49:40 +0300

    components and images

---
+++ homework/src/app/socnet-group/socnet-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'homework-socnet-group',
+  templateUrl: './socnet-group.component.html',
+  styleUrls: ['./socnet-group.component.css']
+})
+export class SocnetGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ homework/src/app/temperature/temperature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'homework-temperature',
+  templateUrl: './temperature.component.html',
+  styleUrls: ['./temperature.component.css']
+})
+export class TemperatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
